fe7be6cf89e2dd6ed7c407ef77bd870f69203bf4
Jade <jademfcoelho@gmail.com>
2024-12-26 23:54:02 -0300

    estilizando header, footer, filter e list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FilterComponent } from '../../components/filter/filter.component';
+import { ListComponent } from '../../components/list/list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FilterComponent, ListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
